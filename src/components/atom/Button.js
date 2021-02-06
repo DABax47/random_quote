ca29fbe218303f5092573e7b9ce0e19702ee3cf5
DABax47 <45886982+DABax47@users.noreply.github.com>
2021-02-06 08:04:40 -0500

    init

---
+++ src/components/atom/Button.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const Button = () => (
+  <Fragment>
+    <button>Prev</button>
+    <button>Next</button>
+  </Fragment>
+);
+
+export default Button;
---
+++ src/components/quote/Quote.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+const Quote = () => (
+  <Fragment>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque porro</p>
+  </Fragment>
+);
+
+export default Quote;
